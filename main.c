33633ebb277a050208b13a415673949ca66ebba4
MartijnVerkleij <mail@mverkleij.nl>
2014-10-31 09:37:19 +0100

    boohoo, geen werkende wiringpi op desktop

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 extern void *init(char *);
 extern void *keyboardHandler();
 extern void *inputHandler();
+int tick();
 
 pthread_t keyboardThread;
 pthread_t inputThread;
@@ -15,12 +16,19 @@ int main( int argc, char *argv[] ) {
 	
 	init("The Endgame");
 	int targ = 0;
+	Game game;
 	pthread_create(&keyboardThread, NULL, &keyboardHandler, &targ);
-	pthread_create(&inputThread, NULL, &inputHandler, &targ);
+	pthread_create(&inputThread, NULL, &inputHandler, &game);
 	while(1){
+		SDL_AddTimer(10, 
+
+
 		puts("====Tick====");
 		SDL_Delay((Uint32) 1000/60);
 	}
 
 }
 
+int tick() {
+	
+}
